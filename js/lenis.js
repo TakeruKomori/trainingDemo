e383340e777d37195d0768dc22eb39f8b257c881
Takeru Komori <komori@strdesign.ac>
2025-06-02 12:24:43 +0900

    12:30

---
+++ js/lenis.js
@@ -0,0 +1,31 @@
+
+const lenis = new Lenis({
+  duration: 2.2,
+  easing: (t) => Math.min(1, 1.01 - Math.pow(2, -6 * t)),
+// スムースな加速・減速
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+lenis.on("scroll", () => {
+  const elements = document.querySelectorAll('.js-parallax');
+  const windowHeight = window.innerHeight;
+
+  elements.forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < windowHeight && rect.bottom > 0) {
+      const ratio = rect.top / windowHeight;
+      const offset = ratio * 60;
+      el.style.transform = `translateY(${offset}px)`;
+    }
+  });
+
+});
+
+
